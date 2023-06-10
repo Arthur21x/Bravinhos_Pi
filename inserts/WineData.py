33308c10bf37a526b44f09f52f5e700b44c470de
Arthur21x <86072531+Arthur21x@users.noreply.github.com>
2023-06-10 12:22:37 -0300

    insert na Tabela WineDetails

--- inserts/WineData.py
+++ inserts/WineData.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import sqlite3
 
-df_winedata = pd.read_csv('C:/Users/Aluno/PycharmProjects/Bravinhos/Bravinhos_Pi/base/static/WineData.csv', sep=';')
+df_winedata = pd.read_csv('C:/Users/ACER/PycharmProjects/Bravinhos/Bravinhos_Pi/base/static/WineData.csv', sep=';')
 
 winedata = df_winedata[['country', 'description', 'designation', 'points', 'price', 'province', 'region_1', 'region_2',
                         'variety', 'winery']]
 
-conn = sqlite3.connect('db.sqlite3')
+conn = sqlite3.connect('C:/Users/ACER/PycharmProjects/Bravinhos/Bravinhos_Pi/db.sqlite3')
 
 cursor = conn.cursor()
 
-for index in winedata.values:
+for value, index in enumerate(winedata.values):
     cursor.execute(
-        f'''INSERT INTO WineData VALUES ('{index[0]}', '{index[1]}', '{index[2]}', {index[3]}, {index[4]}, '{index[5]}', 
-         '{index[6]}', '{index[7]}', '{index[8]}', '{index[9]}')''')
+        f"INSERT INTO Wine_general_winedata VALUES ({value+1}, '{index[0]}', '{index[1]}', '{index[2]}', {index[3]}, "
+        f"{index[4]}, '{index[5]}','{index[6]}', '{index[7]}', '{index[8]}', '{index[9]}')")
+    conn.commit()
+
+cursor.close()
+conn.close()
