d4ffb81faa64f0b8c1f5076df35409ac69637cbe
Arthur21x <86072531+Arthur21x@users.noreply.github.com>
2024-04-03 11:08:07 -0300

    Deploy Microsoft Azure

--- Bravinhos/settings.py
+++ Bravinhos/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATICFILE_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base' / 'static'
