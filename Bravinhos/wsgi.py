d07e1785f1f02410ef94b44a10fa36d80d6767be
Arthur21x <86072531+Arthur21x@users.noreply.github.com>
2024-04-06 18:35:51 -0300

    Deploy V5 Microsoft Azure

--- Bravinhos/wsgi.py
+++ Bravinhos/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bravinhos.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'Bravinhos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bravinhos.deployment')
 
 application = get_wsgi_application()
