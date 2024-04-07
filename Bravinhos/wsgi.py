d61bb12546d521d43585c04766efd065b7020966
Arthur21x <86072531+Arthur21x@users.noreply.github.com>
2024-04-06 21:45:54 -0300

    Deploy V6 Microsoft Azure

--- Bravinhos/wsgi.py
+++ Bravinhos/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bravinhos.deployment')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bravinhos.settings')
 
 application = get_wsgi_application()
